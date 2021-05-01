35bd49b46f85866773ff05dd1204d672a1e9677b
Erick Flore <ferick8246@gmail.com>
2021-04-30 22:56:59 -0500

    adde discription

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-// TODO: Include packages needed for this application
-console.log("hello world")
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+//const questions = [];
+const teLaDejoCaer = (answers) =>
+`
+## ${answers.title} 
+<br/>
+
+## Description 
+${answers.description}
+<br/>
+${answers.license}
+<br/>
+
+## Table of Contents
+
+
+`
+inquirer
+  .prompt([
+    { type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    { type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    { type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    { type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    { type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    { type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
